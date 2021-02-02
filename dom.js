0766c1959b464af8b3064a405841a46291beb9c2
Swann Martin <swannmartin1988@gmail.com>
2021-02-02 15:00:42 +0100

    new file:   dom.js 	new file:   img/background.jpg 	new file:   img/chat_1.jpg 	new file:   img/chat_2.jpg 	new file:   img/chat_3.jpg 	new file:   img/chat_4.jpg 	new file:   img/chat_5.jpg 	new file:   img/tete-chat.png 	new file:   index.html new file:   polices-couleurs.txt 	new file:   style.css

---
+++ dom.js
@@ -0,0 +1,25 @@
+$("form").on("submit", e => {
+  e.preventDefault();
+
+  // condition pour rendre les borders du select en vert si une valeur du select a bien été séléctionnée, en rouge si aucune valeur n'est séléctionnée.
+
+  $("select").val()
+    ? $("select").removeClass("border-danger").addClass("border-success")
+    : $("select").addClass("border-danger");
+
+  // condition pour rendre les borders du textarea en vert si un texte de plus de 15 caratères a bien été écrit, en rouge si ce n'est pas le cas.
+
+  $("textarea").val().length > 15
+    ? $("textarea").removeClass("border-danger").addClass("border-success")
+    : $("textarea").addClass("border-danger");
+
+  // si les deux conditions sont remplies, le form disparaît et une alert valide informe l'utilisateur..
+
+  if ($("textarea").val().length > 15 && $("select").val()) {
+    $("form").remove();
+    $(".middle__left")
+      .append(`<div class="alert alert-success mt-4" role="alert">
+    Votre demande à bien été envoyé! Nous vous répondrons dans les meilleurs délais</a>
+  </div>`);
+  }
+});
